88f7448e30a0be79af9975f3e153bc821f1f7cd0
Clider Fernando Tutaya Rivera <clidertutayarivera@gmail.com>
2026-03-06 22:55:47 -0500

    fix: Set basePath to /portfolio for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   // Si tu repositorio no se llama <tu-usuario>.github.io (por ejemplo, si se llama "mi-cv"), 
   // descomenta la siguiente línea y reemplaza con el nombre de tu repositorio:
-  // basePath: '/mi-cv',
+  basePath: '/portfolio',
   images: {
     unoptimized: true, // Importante para GitHub Pages ya que no tiene un servidor para optimizar imágenes
   }
